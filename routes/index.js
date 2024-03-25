79a55e9df682761fb8cdeecc945ca28a76bd1f6b
Brian Bawden <brianbawden001@gmail.com>
2024-03-25 13:49:11 -0400

    add route for transactions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const users = require('./users');
+const transactions = require('./transactions.js');
 
 let docData;
 
 // TO ROUTES
 router.use('/users', users);
+router.use('/transactions', transactions);
 
 // TO OAUTH
 router.use('/', require('./oauth.js'));
