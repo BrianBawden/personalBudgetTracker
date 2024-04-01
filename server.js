d82d0c0f3f4064df1bcd8b445edbd14f437695bb
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-04-01 10:44:12 -0300

    REFACTOR file separation app.js and server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+const app = require('./app');
+const connection = require('./db/connect');
+
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+const render = process.env.RENDER_URI;
+
+connection.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+    console.log(`API documentation - localhost: http://localhost:${port}/api-docs`);
+    console.log(`API documentation - Production server: ${render}/api-docs`);
+  }
+});
